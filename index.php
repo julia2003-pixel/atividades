48680e21d67a80744df71cb188c787d530e7aacc
julia2003-pixel <juliaregina03@outlook.com>
2020-12-18 19:21:15 -0300

    atualizando conteudo

--- index.php
+++ index.php
@@ -1,15 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-    <meta charset="utf-8" />
-    <title>Pagina Inicial com Bootstrap</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-    
+    <meta charset="UTF-8">
+    <title>Link para minhas atividades</title>
 </head>
 <body>
-    <div class="row">
-        <h1> Pagina com links das minhas atividades </h1>
-    </div class>
+    <h1>Link para minhas atividades:</h1>
+    <p>
+        <h3>Dia 11/08/2020:</h3>
+        <a href="20200811/index.php">Editor de texto com JQuery</a>
+    </p>
+    <p>
+        <h3>Dia 19/08/2020:</h3>
+        <a href="2020.08.19/index.php">Engenharia Reversa</a>
+    </p>
+    <p>
+        <h3>Dia 26/08/2020:</h3>
+        <a href="20200826.individual/exe_ajax.php">Exercício ajax</a>
+        <a href="20200826/index.php">Exercício ajax</a>
+    </p>
+    <p>
+        <h3>Dia 16/09/2020:</h3>
+        <a href="20200916/form.php">API VIACEP</a>
+    </p>
+    <p>
+        <h3>Projeto Primeiro Bimestre:</h3>
+        <a href="projeto/grupob.php">Projeto</a>
+    </p>
+    <p>
+        <h3>Dia 30/09/2020 Filtro com select e inner join:</h3>
+        <a href="20200930/especie.php">Taxonomia- Tabela especies de animais || </a>
+        <a href="20200930/familia.php">Taxonomia- Tabela familias de animais || </a>
+        <a href="20200930/genero.php">Taxonomia- Tabela genero de animais</a>
+    </p>
+    <p>
+        <h3>Dia 14/10/2020:</h3>
+        <a href="20201014/index.php">Taxonomia cadastro, filtro e listagem</a>
+    </p>
+    <p>
+        <h3>Projeto Segundo Bimestre:</h3>
+        <a href="Projeto_2Bim/Projeto_Christian_JuliaRegina_2Bim/index.php">Projeto</a>
+    </p>
+    <p>
+        <h3>Projeto Acumulativo Terceiro Bimestre:</h3>
+        <a href="20201104-Projeto_3_bimestre/Projeto/index.php">Projeto</a>
+    </p>
 </body>
 </html>
